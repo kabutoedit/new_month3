2258b8978cb694fe36c624a86744ea14d1d0160d
kabutoedit <149598256+kabutoedit@users.noreply.github.com>
2023-11-18 13:57:01 +0600

    home work 3

--- js/lesson.js
+++ js/lesson.js
@@ -17,3 +17,52 @@ phoneButton.addEventListener('click',() => {
         phoneSpan.style.color = 'red'
     }
 })
+
+//TAB SLIDER
+
+const tabsContendCards = document.querySelectorAll('.tab_content_block')
+const tabsItems = document.querySelectorAll('.tab_content_item')
+const tabsItemsParent = document.querySelector('.tab_content_items')
+let currentIndex = 0
+
+const hideTabsContentCards = () => {
+    tabsContendCards.forEach((tabContendCard) => {
+        tabContendCard.style.display = 'none'
+    })
+    tabsItems.forEach((tabsItem) => {
+        tabsItem.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabsContentCards = (indexElement = 0) => {
+    tabsContendCards[indexElement].style.display = 'block'
+    tabsItems[indexElement].classList.add('tab_content_item_active')
+}
+
+hideTabsContentCards()
+showTabsContentCards()
+
+tabsItemsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabsItems.forEach((tabsItem,tabsItemIndex) => {
+            if (event.target === tabsItem) {
+                hideTabsContentCards()
+                showTabsContentCards(tabsItemIndex)
+                currentIndex = tabsItemIndex
+            }
+        })
+    }
+}
+
+const changeTab = () => {
+    currentIndex = (currentIndex + 1) % tabsItems.length;
+    hideTabsContentCards()
+    showTabsContentCards(currentIndex)
+}
+
+setInterval(changeTab, 5000)
+
+
+
+
+
