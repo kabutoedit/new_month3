9456e101e85013e789995208ccd1c22ac9ac751c
kabutoedit <149598256+kabutoedit@users.noreply.github.com>
2023-12-02 12:59:51 +0600

    hw7

--- js/lesson.js
+++ js/lesson.js
@@ -70,24 +70,26 @@ const usd = document.querySelector('#usd')
 const cny = document.querySelector('#cny')
 
 const converter = (element, targetElement, targetElement2, type) => {
-    element.oninput = () => {
-        fetch('../data/converter.json')
-            .then(response => response.json())
-            .then(data => {
-                if (type === 'som') {
-                    targetElement.value = (element.value / data.usd).toFixed(2)
-                    targetElement2.value = (element.value * data.von).toFixed(2)
-                } else if (type === 'usd') {
-                    targetElement.value = (element.value *  data.usd).toFixed(2)
-                    targetElement2.value = (element.value * data.usdToVon).toFixed(2)
-                } else if (type === 'cny') {
-                    targetElement.value = (element.value / data.usdToVon).toFixed(2)
-                    targetElement2.value = (element.value / data.von).toFixed(2)
-                }
+    element.oninput = async (event) => {
+        try {
+            const response = await fetch('../data/converter.json')
+            const data = await response.json()
+            if (type === 'som') {
+                targetElement.value = (element.value / data.usd).toFixed(2)
+                targetElement2.value = (element.value * data.von).toFixed(2)
+            } else if (type === 'usd') {
+                targetElement.value = (element.value *  data.usd).toFixed(2)
+                targetElement2.value = (element.value * data.usdToVon).toFixed(2)
+            } else if (type === 'cny') {
+                targetElement.value = (element.value / data.usdToVon).toFixed(2)
+                targetElement2.value = (element.value / data.von).toFixed(2)
+            }
                 element.value === '' && (targetElement.value = '')
                 element.value === '' && (targetElement2.value = '')
-            })
-            .catch(error => console.error('Error fetching data:', error));
+        }
+        catch (e) {
+            console.log(event)
+        }
     }
 }
 
@@ -108,50 +110,129 @@ const card = document.querySelector('.card'),
 let count = 1
 
 
-
-btnNext.onclick = () => {
+btnNext.onclick = async (event) => {
     count++
     if (count > 200) {
         count = 1
     }
-    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
-        .then(response => response.json())
-        .then(data => {
-             card.innerHTML = `
-                <p>${data.title}</p>
-                <p style=" color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
-                <span>${data.id}</span>`
-        })
-}
-
-btnPrev.onclick = () =>{
-    count--
-    if (count < 1) {
-        count = 200
-    }
-    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
-        .then(response => response.json())
-        .then(data => {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+        const data = await response.json()
             card.innerHTML = `
                 <p>${data.title}</p>
                 <p style=" color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
                 <span>${data.id}</span>`
-        })
+    }
+    catch (e) {
+        console.log(e)
+    }
+
+    // Старый fetch запрос
+
+    // fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //          card.innerHTML = `
+    //             <p>${data.title}</p>
+    //             <p style=" color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
+    //             <span>${data.id}</span>`
+    //     })
 }
 
-fetch(`https://jsonplaceholder.typicode.com/todos/1`)
-    .then(response => response.json())
-    .then(data => {
+btnPrev.onclick = async (event) => {
+    count--
+    if (count < 1) {
+        count = 200
+    }
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+        const data = await response.json()
         card.innerHTML = `
-                <p>${data.title}</p>
-                <p style=" color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
-                <span>${data.id}</span>`
-    })
+            <p>${data.title}</p>
+            <p style=" color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
+            <span>${data.id}</span>`
+    }
+    catch (e) {
+        console.log(e)
+    }
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => {
+    // Старый fetch запрос
+
+    // fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         card.innerHTML = `
+    //             <p>${data.title}</p>
+    //             <p style=" color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
+    //             <span>${data.id}</span>`
+    //     })
+}
+
+const cardDefalt = async (event) => {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+        const data = await response.json()
+        card.innerHTML = `
+            <p>${data.title}</p>
+            <p style=" color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
+            <span>${data.id}</span>`
+    }
+    catch (e) {
+        console.log(event)
+    }
+}
+
+cardDefalt()
+
+// Старый fetch запрос
+
+// fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+//     .then(response => response.json())
+//     .then(data => {
+//         card.innerHTML = `
+//                 <p>${data.title}</p>
+//                 <p style=" color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
+//                 <span>${data.id}</span>`
+//     })
+
+const responseOnJsonplaseholder = async (event) => {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const data = await response.json()
         console.log(data)
-    })
+    }
+    catch (e) {
+        console.log(event)
+    }
+}
+
+responseOnJsonplaseholder()
+
+// Старый fetch запрос
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//     })
 
 
+// WEATHER
+
+const cityNameInput = document.querySelector('.cityName')
+const city = document.querySelector('.city')
+const temp = document.querySelector('.temp')
+
+const WEATHER_API = 'http://api.openweathermap.org/data/2.5/weather'
+const API_KEY = 'e417df62e04d3b1b111abeab19cea714'
+
+cityNameInput.oninput = async (event) => {
+    try {
+        const response = await fetch(`${WEATHER_API}?q=${event.target.value}&appid=${API_KEY}`)
+        const data = await response.json()
+        city.innerHTML = data?.name ? data?.name : 'Город не найден...'
+        temp.innerHTML = data?.main?.temp ? Math.round(data?.main?.temp - 273) + '&deg;C' : '...'
+    } catch (e) {
+        console.log(e)
+    }
+}
